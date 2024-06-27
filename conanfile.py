8675269618475b5324b6620d224665729e8b7afb
Elvis Dukaj <elvis.dukaj@gmail.com>
2024-06-27 18:03:26 +0200

    added parsing option ang get the image

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,27 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
-
-class img2asciiRecipe(ConanFile):
+class Image2AsciiRecipe(ConanFile):
     name = "img2ascii"
     version = "1.0"
-    package_type = "application"
 
     # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of img2ascii package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "MIT"
+    author = "Elvis Dukaj"
+    url = ""
+    description = "A simple program to convert image to ascii art"
+    topics = ("image", "ascii-art")
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "CMakeLists.txt", "src/*"
+
+    def requirements(self):
+        self.requires("cpr/1.10.5")
+        self.requires("cxxopts/3.2.0")
+        self.requires("fmt/10.2.1")
 
     def layout(self):
         cmake_layout(self)
@@ -33,11 +36,3 @@ class img2asciiRecipe(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    
-
-    
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
-#include "img2ascii.h"
-#include <vector>
+#include <cxxopts.hpp>
+#include <fmt/core.h>
+#include <cpr/cpr.h>
+
 #include <string>
 
-int main() {
-    img2ascii();
+int main(int argc, char* argv[]) {
 
-    std::vector<std::string> vec;
-    vec.push_back("test_package");
+  cxxopts::Options options("img2ascii", "Convert images to ASCII art");
 
-    img2ascii_print_vector(vec);
+  options.add_options()
+     ("h,help", "Help")
+     ("u,url", "URL of the image to convert to ASCII art", cxxopts::value<std::string>()->default_value("https://cppusergroupvienna.org/wp-content/uploads/2024/05/cpp_usergroup_vienna_bright_300dpi.png"))
+     ("w,width", "Width in characters of the output image", cxxopts::value<int>()->default_value("80"));
+
+  auto result = options.parse(argc, argv);
+  if(result.count("help")) {
+    fmt::println("{}", options.help());
+    exit(0);
+  }
+
+  cpr::Response r = cpr::Get(cpr::Url{result["url"].as<std::string>()});
+
+  fmt::println("Status Code: {}", r.status_code);
+
+  return 0;
 }
