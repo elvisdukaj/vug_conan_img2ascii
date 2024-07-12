944d33a126a599008c10b834187b10942b75eba1
Elvis Dukaj <elvis.dukaj@gmail.com>
2024-07-12 13:58:23 +0200

    Removing std library

--- conanfile.py
+++ conanfile.py
@@ -8,21 +8,17 @@ class Image2AsciiRecipe(ConanFile):
     # Optional metadata
     license = "MIT"
     author = "Elvis Dukaj"
-    url = ""
+    url = "https://github.com/elvisdukaj/vug_conan_img2ascii"
     description = "A simple program to convert image to ascii art"
     topics = ("image", "ascii-art")
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*"
-
     def requirements(self):
         self.requires("cpr/1.10.5")
         self.requires("cxxopts/3.2.0")
         self.requires("fmt/10.2.1")
-        self.requires("stb/cci.20240213")
         self.requires("opencv/4.9.0")
 
     def layout(self):
